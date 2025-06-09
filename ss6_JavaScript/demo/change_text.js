48c3a0f0d6961cd4392d3a13687941ef8cbc299c
VanThanh34 <vvt1304@gmail.com>
2025-06-09 15:45:47 +0700

    bài tập cờ vua

---
+++ ss6_JavaScript/demo/change_text.js
@@ -0,0 +1,7 @@
+function change_text(){
+    document.getElementById("change").innerHTML = "Đổi rồi cu!!!"
+}
+
+function reset(){
+    document.getElementById("change").innerHTML = "Chuyển lại nè!!!"
+}
