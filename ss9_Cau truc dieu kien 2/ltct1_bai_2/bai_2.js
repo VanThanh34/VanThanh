9c60140942972a0eb78039a431f36525addd1307
VanThanh34 <vvt1304@gmail.com>
2025-06-12 10:14:52 +0700

    bài tập ltct1 bài 2

---
+++ ss9_Cau truc dieu kien 2/ltct1_bai_2/bai_2.js
@@ -0,0 +1,12 @@
+let tuoi = document.getElementById("tuoi");
+let btn = document.getElementById("btn");
+btn.addEventListener("click", onClick)
+
+    function onClick(){
+        if(+tuoi.value > 15){
+            document.getElementById("answer").innerHTML=("Bé đã đủ vào lớp 10 rồi!");
+        } else {
+            document.getElementById("answer").innerHTML=("Về ăn cho chóng lớn rồi vào lớp 10.");
+        }
+    }
+
