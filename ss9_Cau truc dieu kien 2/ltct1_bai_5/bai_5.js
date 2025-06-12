7051aec2c39892c41a929552be935512a9f279a5
VanThanh34 <vvt1304@gmail.com>
2025-06-12 11:19:51 +0700

    bài tập học lực

--- ss9_Cau truc dieu kien 2/ltct1_bai_5/bai_5.js
+++ ss9_Cau truc dieu kien 2/ltct1_bai_5/bai_5.js
@@ -2,9 +2,7 @@ let num1 = +prompt("Nhập điểm bài kiểm tra!");
 let num2 = +prompt("Nhập điểm bài thi giữa kì!");
 let num3 = +prompt("Nhập điểm bài kiểm tra!");
 let tong = (num1 + num2 + num3)/3;
-if( num1 >= 0 && num1 <= 10 &&
-    num2 >= 0 && num2 <= 10 &&
-    num3 >= 0 && num3 <= 10) {
+if( 0 <= tong && tong >= 10) {
     if (tong > 8) {
         alert("Bạn đạt loại giỏi!!!");
     } else if (tong >= 6.5 && tong <=8 ) {
