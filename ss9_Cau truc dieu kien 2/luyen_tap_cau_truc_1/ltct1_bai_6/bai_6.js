d26674e4964896499a1667a0e46f3f5749369d87
VanThanh34 <vvt1304@gmail.com>
2025-06-12 13:35:55 +0700

    bài tập tính hoa hồng

--- ss9_Cau truc dieu kien 2/luyen_tap_cau_truc_1/ltct1_bai_6/bai_6.js
+++ ss9_Cau truc dieu kien 2/luyen_tap_cau_truc_1/ltct1_bai_6/bai_6.js
@@ -3,6 +3,7 @@ let hoaHong = amount * 0.03;
 
 if (amount === 0 || amount === "" || isNaN(amount) ) {
     alert("Số tiền bạn nhập không hợp lê.");
-} else {
+} else
+{
     alert("Số tiền hoa hồng nhận được: "+ hoaHong);
 }
