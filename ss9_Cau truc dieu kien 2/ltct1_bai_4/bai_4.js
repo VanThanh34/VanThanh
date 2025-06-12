c61bc09f78c6c673b786c862d57c57d48bbace79
VanThanh34 <vvt1304@gmail.com>
2025-06-12 10:33:06 +0700

    bài tập ltct1 bài 4

---
+++ ss9_Cau truc dieu kien 2/ltct1_bai_4/bai_4.js
@@ -0,0 +1,15 @@
+let num1 = +prompt("Mời nhập số a");
+let num2 = +prompt("Mời nhập số b");
+let num3 = +prompt("Mời nhập số c");
+
+if (num1 > num2) {
+    if (num1 > num3) {
+        alert("Số lớn nhất là số " + num1);
+    } else {
+        alert("Số lớn nhất là số " + num3);
+    }
+} else if (num2 > num3) {
+    alert("Số lớn nhất là số " + num2);
+} else {
+    alert("Số lớn nhất là số " + num3);
+}
