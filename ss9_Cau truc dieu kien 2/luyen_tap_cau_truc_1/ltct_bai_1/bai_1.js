7912e9ab6f95ce303352f7343c968f353bc9b04b
VanThanh34 <vvt1304@gmail.com>
2025-06-12 13:33:06 +0700

    bài tập số a chia hết cho số b

--- ss9_Cau truc dieu kien 2/luyen_tap_cau_truc_1/ltct_bai_1/bai_1.js
+++ ss9_Cau truc dieu kien 2/luyen_tap_cau_truc_1/ltct_bai_1/bai_1.js
@@ -2,6 +2,7 @@ let num1 = +prompt("Mời nhập số a");
 let num2 = +prompt("Mời nhập số b");
 if (num1%num2 === 0) {
     alert("Số a chia hết cho số b");
-} else{
+} else
+{
     alert("Số a không chia hết cho số b");
 }
