05a7aa9c9f2f946ab502e1cb11f0b5a32f1ca8ff
VanThanh34 <vvt1304@gmail.com>
2025-06-13 16:33:49 +0700

    sửa bài tập ltct1_bai6

--- ss9_Cau truc dieu kien 2/luyen_tap_cau_truc_1/ltct1_bai_6/bai_6.js
+++ ss9_Cau truc dieu kien 2/luyen_tap_cau_truc_1/ltct1_bai_6/bai_6.js
@@ -1,9 +1,8 @@
 let amount = +prompt("Nhập số tiền căn hộ", "");
-let hoaHong = amount * 0.03;
 
-if (amount === 0 || amount === "" || isNaN(amount) ) {
-    alert("Số tiền bạn nhập không hợp lê.");
-} else
-{
-    alert("Số tiền hoa hồng nhận được: "+ hoaHong);
+if (amount === 0  || isNaN(amount) ) {
+    alert("Số tiền bạn nhập không hợp lệ.");
+} else {
+    let receiveCommission = amount * 0.03;
+    alert("Số tiền hoa hồng nhận được: "+ receiveCommission);
 }
