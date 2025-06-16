3d98527059840d950fc629cd04923b266146bb83
VanThanh34 <vvt1304@gmail.com>
2025-06-16 10:04:03 +0700

    bài tập ltvl1_bai2

---
+++ ss10_Vong_lap/luyen_tap_vong_lap_1/bai_2/bai_2.js
@@ -0,0 +1,6 @@
+let temp = +prompt(`Mời nhập vào thông tin nhiệt độ`);
+if (temp < 20) {
+    alert(`Bạn hãy tăng nhiệt độ, ở đây lạnh quá`);
+} else if (temp >100){
+    alert(`Bạn hãy giảm nhiệt độ, ở đây như địa ngục`);
+}
