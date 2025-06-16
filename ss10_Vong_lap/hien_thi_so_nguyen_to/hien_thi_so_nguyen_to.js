c8702b4984fd1481210e2b70b8d7bf1f138480b4
VanThanh34 <vvt1304@gmail.com>
2025-06-16 09:47:00 +0700

    bài tập hiển thị bao nhiêu số nguyên tố

---
+++ ss10_Vong_lap/hien_thi_so_nguyen_to/hien_thi_so_nguyen_to.js
@@ -0,0 +1,19 @@
+let num = +prompt("Mời nhập số lượng cần in ra");
+let count = 0;
+let n = 2;
+
+while (count < num) {
+    let primeNum = true;
+    //chỉ kiểm tra đến căn bậc 2 của n vì nếu n chia hết cho số nào lớn hơn căn bậc hai của n thì nó đã chia hết cho 1 số nhỏ hơn rồi
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            primeNum = false;
+            break;
+        }
+    }
+    if (primeNum) {
+        alert(`Số nguyên tố thứ ${count + 1}: ${n}`)
+        count++;
+    }
+    n++;
+}
