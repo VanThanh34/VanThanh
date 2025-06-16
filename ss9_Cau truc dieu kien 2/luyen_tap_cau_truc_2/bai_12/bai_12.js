203c50b6f63d667928de8493927aa310b2408505
VanThanh34 <vvt1304@gmail.com>
2025-06-16 16:36:41 +0700

    bài tập ltct2_bài 12

--- ss9_Cau truc dieu kien 2/luyen_tap_cau_truc_2/bai_12/bai_12.js
+++ ss9_Cau truc dieu kien 2/luyen_tap_cau_truc_2/bai_12/bai_12.js
@@ -1,7 +1,9 @@
-let i = 1;
-
-while (i < 100) {
-    //phân thân vòng lặp
-    document.write("<hr width = " + i + "%>");
-    i++;
+let tienGui =+prompt(`Nhập số tiền gửi`);
+let laiSuatMotThang =+prompt(`Nhập lãi suất 1 tháng`);
+let soThang =+prompt(`Nhập số tháng gửi`);
+let tienLaiThang =0;
+for (let i = 1; i <=soThang ; i++) {
+    tienLaiThang = tienGui*laiSuatMotThang/100;
+    tienGui +=tienLaiThang;
 }
+console.log(`tổng tiền gưi sau ${soThang} tháng là ${tienGui}`);
