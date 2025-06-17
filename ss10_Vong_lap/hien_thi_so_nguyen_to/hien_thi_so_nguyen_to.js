17f18c719dfba6b1e394c095798310ff4821e991
VanThanh34 <vvt1304@gmail.com>
2025-06-17 08:54:09 +0700

    sửa btap ltvl1_bài hiển thị số nguyên tố đầu tiên

--- ss10_Vong_lap/hien_thi_so_nguyen_to/hien_thi_so_nguyen_to.js
+++ ss10_Vong_lap/hien_thi_so_nguyen_to/hien_thi_so_nguyen_to.js
@@ -1,9 +1,8 @@
 let num = +prompt("Mời nhập số lượng cần in ra");
 let count = 0;
 let n = 2;
-
+let primeNum = true;
 while (count < num) {
-    let primeNum = true;
     //chỉ kiểm tra đến căn bậc 2 của n vì nếu n chia hết cho số nào lớn hơn căn bậc hai của n thì nó đã chia hết cho 1 số nhỏ hơn rồi
     for (let i = 2; i <= Math.sqrt(n); i++) {
         if (n % i === 0) {
---
+++ ss12_Mang/demo/demo.js
@@ -0,0 +1,7 @@
+let arrNum = [1,2,3];
+let arrStr = ["one", "two", "three"];
+let arrMix = ["1","two", "3", "four"];
+
+for (let i = 0; i < arrStr.length; i++) {
+    document.write(`${arrStr[i]} `);
+}
