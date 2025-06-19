2de51db1f09c0982ee2c84fbc88b7fad8377070b
VanThanh34 <vvt1304@gmail.com>
2025-06-19 14:13:04 +0700

    sửa btap ltctdk2 bài 5

--- ss9_Cau truc dieu kien 2/luyen_tap_cau_truc_2/bai_5/bai_5.js
+++ ss9_Cau truc dieu kien 2/luyen_tap_cau_truc_2/bai_5/bai_5.js
@@ -2,7 +2,7 @@ let num1 = +prompt("Nhập chiều dài cạnh a");
 let num2 = +prompt("Nhập chiều dài cạnh b");
 let dTich = (num1*num2)/2;
 
-if (dTich > 0 && !isNaN(dTich)){
+if (num1 > 0 && !isNaN(num1) && num2 > 0 && !isNaN(num2)){
     alert("Diện tích tam giác là: " +dTich +"m2");
 } else {
     alert("Mời bạn nhập lại");
