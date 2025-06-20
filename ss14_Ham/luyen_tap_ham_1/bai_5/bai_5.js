1c2006820e86e2929e36af23fb450f018e8a4865
VanThanh34 <vvt1304@gmail.com>
2025-06-20 08:57:40 +0700

    bài tập lth1 bài tập 5

---
+++ ss14_Ham/luyen_tap_ham_1/bai_5/bai_5.js
@@ -0,0 +1,16 @@
+let arrStars1 = ["Polaris", "Aldebaran", "Deneb", "Vega", "Altair", "Dubhe", "Regulus"];
+let arrStars2 = ["Ursa Minor", "Taurus", "Cygnus", "Lyra", "Aquila", "Ursa Major", "Leo"];
+function findStar() {
+    let str = document.getElementById("star").value;
+    let found = false;
+    for (let i = 0; i < arrStars1.length; i++) {
+        if (str === arrStars1[i]) {
+            document.getElementById("answer").innerHTML = (`Result: ${arrStars2[i]}`);
+        found = true;
+        break;
+        }
+    }
+    if(!found){
+        document.getElementById("answer").innerHTML = (`No result for ur star :<`);
+    }
+}
