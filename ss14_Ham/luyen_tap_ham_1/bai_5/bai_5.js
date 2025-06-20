83cdf198596d417671aa6a1d3e7cf36ea5b75b7d
VanThanh34 <vvt1304@gmail.com>
2025-06-20 10:35:54 +0700

    cập nhật bài tập lth1 bài 5

--- ss14_Ham/luyen_tap_ham_1/bai_5/bai_5.js
+++ ss14_Ham/luyen_tap_ham_1/bai_5/bai_5.js
@@ -1,16 +1,17 @@
 let arrStars1 = ["Polaris", "Aldebaran", "Deneb", "Vega", "Altair", "Dubhe", "Regulus"];
 let arrStars2 = ["Ursa Minor", "Taurus", "Cygnus", "Lyra", "Aquila", "Ursa Major", "Leo"];
+
 function findStar() {
     let str = document.getElementById("star").value;
     let found = false;
     for (let i = 0; i < arrStars1.length; i++) {
-        if (str === arrStars1[i]) {
+        if (str.toLowerCase() === arrStars1[i].toLowerCase()) {
             document.getElementById("answer").innerHTML = (`Result: ${arrStars2[i]}`);
-        found = true;
-        break;
+            found = true;
+            break;
         }
     }
-    if(!found){
+    if (!found) {
         document.getElementById("answer").innerHTML = (`No result for ur star :<`);
     }
 }
