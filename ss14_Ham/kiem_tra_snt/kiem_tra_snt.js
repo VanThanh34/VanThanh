3f2c5d6fc96e64c6ee56786121eea5bd157f8da6
VanThanh34 <vvt1304@gmail.com>
2025-06-23 09:11:40 +0700

    sửa bài tập snt

--- ss14_Ham/kiem_tra_snt/kiem_tra_snt.js
+++ ss14_Ham/kiem_tra_snt/kiem_tra_snt.js
@@ -1,6 +1,6 @@
 let n = +prompt("Mời nhập 1 số");
-function primeNum(){
-    for (let i = 2; i < Math.sqrt(n) ; i++) {
+function primeNum(n){
+    for (let i = 2; i <= Math.sqrt(n) ; i++) {
         if(n%i === 0) {
             alert(`Số ${n} không phải là số nguyên tố`);
             return;
@@ -9,7 +9,7 @@ function primeNum(){
     alert(`Số ${n} là số nguyên tố.`);
 }
 if(!isNaN(n) && n >1){
-    primeNum();
+    primeNum(n);
 }else {
     alert("Mời nhập số hợp lệ!")
 }
