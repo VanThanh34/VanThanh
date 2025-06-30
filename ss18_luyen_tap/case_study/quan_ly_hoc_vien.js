528b2fa94c2588dcbf03666130cb13a0446d3007
VanThanh34 <vvt1304@gmail.com>
2025-06-30 10:17:08 +0700

    Case Study Quản lý học viên

--- ss18_luyen_tap/case_study/quan_ly_hoc_vien.js
+++ ss18_luyen_tap/case_study/quan_ly_hoc_vien.js
@@ -150,7 +150,7 @@ function timKiemHocVien() {
 function formatNgay(ngayISO) {
     let date = new Date(ngayISO);
     let dd = String(date.getDate()).padStart(2, '0');
-    let mm = String(date.getMonth() + 1).padStart(2, '0'); // Tháng tính từ 0
+    let mm = String(date.getMonth() + 1).padStart(2, '0');
     let yyyy = date.getFullYear();
     return `${dd}/${mm}/${yyyy}`;
 }
